273de0b44f2272dbdd63d2ef37b24f8ffe6d961b
ChaimOrelowitz <chaim@corsolutions.io>
2026-08-25 01:02:16 -0400

    Route BRC test to live in-app browser

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import BrcLiveTestPage from './BrcLiveTestPage';
 import './styles.css';
+
+const Root = window.location.pathname === '/brc-test' ? BrcLiveTestPage : App;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>
 );
