f76ac07110bab1ac4511ab863d294f9f7c242eec
ChaimOrelowitz <chaim@corsolutions.io>
2026-08-26 13:30:19 -0400

    Add email settings and recovery routes

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,23 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import AdminPage from './AdminPage';
 import BrcLiveTestPage from './BrcLiveTestPage';
+import EmailSettingsPage from './EmailSettingsPage';
+import AccountRecoveryPage from './AccountRecoveryPage';
 import './styles.css';
 import './workflow.css';
 import './brcLive.css';
 import './clientDashboard.css';
 import './intakePolish.css';
-import './intakePolish';
+import './emailSettings.css';
 
 const path = window.location.pathname;
-const Root = path === '/admin' ? AdminPage : path === '/brc-test' ? BrcLiveTestPage : App;
+let Root;
+if (path === '/admin') Root = AdminPage;
+else if (path === '/admin/email-settings') Root = EmailSettingsPage;
+else if (path === '/brc-test') Root = BrcLiveTestPage;
+else if (path === '/forgot-password') Root = () => <AccountRecoveryPage mode="forgot" />;
+else if (path === '/reset-password') Root = () => <AccountRecoveryPage mode="reset" />;
+else Root = App;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
